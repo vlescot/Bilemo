a4e02ad6605b90ea579f8b6d1e76f425a68e5da7
vlescot <vincent.lescot@gmail.com>
2018-11-19 23:04:16 +0100

    Fixes bug. Filter the Phone list by manufacturer's name.

--- src/Domain/Repository/PhoneRepository.php
+++ src/Domain/Repository/PhoneRepository.php
@@ -87,20 +87,13 @@ class PhoneRepository extends ServiceEntityRepository implements
      */
     private function queryFilters(QueryBuilder $qb, array $filters = []): QueryBuilder
     {
-//        dump($filters);
-        if (array_key_exists('manufacturer', $filters) && array_key_exists('model', $filters)) {
-            $qb ->andWhere('p.model LIKE :model')
-                ->setParameter('model', $filters['model'])
-
-                ->leftJoin('p.manufacturer', 'm')
+        if (array_key_exists('manufacturer', $filters)) {
+            $qb ->leftJoin('p.manufacturer', 'm')
                 ->andWhere('m.name LIKE :manufacturer')
                 ->setParameter('manufacturer', $filters['manufacturer']);
+        }
 
-        } elseif (array_key_exists('manufacturer', $filters)) {
-            $qb                 ->leftJoin('p.manufacturer', 'm')
-                ->andWhere('m.name LIKE :manufacturer')
-                ->setParameter('manufacturer', $filters['manufacturer']);
-        } elseif (array_key_exists('model', $filters)) {
+        if (array_key_exists('model', $filters)) {
             $qb ->andWhere('p.model LIKE :model')
                 ->setParameter('model', $filters['model']);
         }
